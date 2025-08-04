b93799be6af83ae88fdcf703c7d685f99674ef86
sebin6962 <157094260+sebin6962@users.noreply.github.com>
2025-08-04 14:31:47 +0900

    도감 수정

--- Capstone_Nemo/Assets/Scripts/DoGam/DoGamData.cs
+++ Capstone_Nemo/Assets/Scripts/DoGam/DoGamData.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+public class RecipeImageData
+{
+    public string tool;                 // 제작기
+    public List<string> ingredients;   // 재료 (최대 4개)
+    public string result;              // 완성된 아이템
+}
+
+[System.Serializable]
 public class DoGamEntry
 {
     public string name;
     public string image;
     public string description;
-    public List<string> recipeImage;
-    public List<string> recipe;
     public string category;
+    public List<string> recipe;
+    public List<string> recipeImage;
+    public List<RecipeImageData> recipeImageBundle;
 }
 
 [System.Serializable]
